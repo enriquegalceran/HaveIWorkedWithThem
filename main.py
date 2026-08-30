d821bcf1f34d767bc7a77252438440515ac42f8b
enriquegalceran <44613764+enriquegalceran@users.noreply.github.com>
2026-08-30 18:29:28 +0200

    Api key reading and processing

--- main.py
+++ main.py
@@ -3,14 +3,29 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import requests
+import json
+import os
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+def main():
+
+    url = "https://api.themoviedb.org/3/authentication"
+
+    with open("api_key", "r") as f:
+        api_key = f.read().strip()
+    headers = {
+        "accept": "application/json",
+        "Authorization": api_key
+    }
+
+    response = requests.get(url, headers=headers)
+
+    print(response.text)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
