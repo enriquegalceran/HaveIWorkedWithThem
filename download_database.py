b8c16ba2f3826057ae4e2f4fb884cdd36add5d87
enriquegalceran <44613764+enriquegalceran@users.noreply.github.com>
2026-09-02 11:12:28 +0200

    download movies as well

--- download_database.py
+++ download_database.py
@@ -65,12 +65,79 @@ def compress_people(people=None, filename=None):
     print("Compressed and saved names, ids, and popularity to .npy files.")
 
 
-def main():
-    people = get_person_export(filename="person_ids.pkl", overwrite=False)
-    compress_people(people=people, filename="person_ids.pkl")
-    print(len(people), people[0])
-    print("here")
+def get_movie_export(date=None, filename=None, overwrite=True):
+    """
+    Downloads TMDB's daily movie ID export.
+    date: datetime object; if None, tries yesterday (today's file
+    may not be published yet, files land ~8am UTC).
+    """
 
+    if filename is not None:
+        assert filename.endswith(".pkl"), "filename must end with .pkl"
+        if os.path.exists(filename) and not overwrite:
+            movies = pickle.load(open(filename, "rb"))
+            return movies
+
+
+    if date is None:
+        date = datetime.utcnow() - timedelta(days=1)
+    date_str = date.strftime("%m_%d_%Y")
+
+    url = f"http://files.tmdb.org/p/exports/movie_ids_{date_str}.json.gz"
+    resp = requests.get(url)
+    resp.raise_for_status()
+
+    with gzip.open(io.BytesIO(resp.content), "rt", encoding="utf-8") as f:
+        movies = [json.loads(line) for line in f]
+
+    if filename is not None:
+        if os.path.exists(filename):
+            if not overwrite:
+                raise FileExistsError(f"{filename} already exists and overwrite is False")
+            else:
+                # Remove file
+                os.remove(filename)
+        with open(filename, "wb") as f:
+            pickle.dump(movies, f)
+
+    return movies
+
+
+def compress_movies(movies=None, filename=None):
+    assert (movies is not None or filename is not None), "Must provide either movies or filename"
+    if movies is None and filename is not None:
+        movies = get_movie_export(filename=filename, overwrite=False)
+    if movies is not None and filename is None:
+        filename = "movie_ids.pkl"
+
+    titles = [_["original_title"] for _ in movies]
+    ids = np.array([_["id"] for _ in movies])
+    popularity = np.array([_["popularity"] for _ in movies])
+
+    filename_titles = filename.replace(".pkl", "_titles.lstb")
+    filename_ids = filename.replace(".pkl", "_ids.npy")
+    filename_popularity = filename.replace(".pkl", "_popularity.npy")
+
+    with open(filename_titles, "wb") as f:
+        for title in titles:
+            f.write(title.encode("utf-8") + b"\n")
+    np.save(filename_ids, ids.astype(np.uint32))
+    np.save(filename_popularity, popularity)
+
+    print("Compressed and saved titles, ids, and popularity to .npy files.")
+
+
+def main():
+    # people = get_person_export(filename="person_ids.pkl", overwrite=False)
+    # compress_people(people=people, filename="person_ids.pkl")
+    # print(len(people), people[0])
+    # print("here")
+
+    movies = get_movie_export(filename="movie_ids.pkl", overwrite=False)
+    compress_movies(movies=movies, filename="movie_ids.pkl")
+    print(len(movies), movies[0])
+    # e.g. 987654 {'id': 238, 'original_title': 'The Godfather', 'popularity': 45.2, 'video': False, 'adult': False}
+    print("here2")
 
 if __name__ == "__main__":
     main()
