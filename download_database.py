f1a84d2b3b56937c52e5a7e4f70de51c801459fd
enriquegalceran <44613764+enriquegalceran@users.noreply.github.com>
2026-09-02 11:05:30 +0200

    compress data

--- download_database.py
+++ download_database.py
@@ -1,6 +1,7 @@
 import requests, gzip, json, io, os
 from datetime import datetime, timedelta
 import pickle
+import numpy as np
 
 
 def get_person_export(date=None, filename=None, overwrite=True):
@@ -40,8 +41,33 @@ def get_person_export(date=None, filename=None, overwrite=True):
     return people
 
 
+def compress_people(people=None, filename=None):
+    assert (people is not None or filename is not None), "Must provide either people or filename"
+    if people is None and filename is not None:
+        people = get_person_export(filename=filename, overwrite=False)
+    if people is not None and filename is None:
+        filename = "person_ids.pkl"
+
+    names = [_["name"] for _ in people]
+    ids = np.array([_["id"] for _ in people])
+    popularity = np.array([_["popularity"] for _ in people])
+
+    filename_names = filename.replace(".pkl", "_names.lstb")
+    filename_ids = filename.replace(".pkl", "_ids.npy")
+    filename_popularity = filename.replace(".pkl", "_popularity.npy")
+
+    with open(filename_names, "wb") as f:
+        for name in names:
+            f.write(name.encode("utf-8") + b"\n")
+    np.save(filename_ids, ids.astype(np.uint32))
+    np.save(filename_popularity, popularity)
+
+    print("Compressed and saved names, ids, and popularity to .npy files.")
+
+
 def main():
     people = get_person_export(filename="person_ids.pkl", overwrite=False)
+    compress_people(people=people, filename="person_ids.pkl")
     print(len(people), people[0])
     print("here")
 
